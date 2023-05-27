4bf15a12862072eb7f91436367805956fcdb021a
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2023-05-27 17:30:14 +1000

    Update ViewModelJumpOffsetEffect.cs

--- Code/ViewModel/Effects/ViewModelJumpOffsetEffect.cs
+++ Code/ViewModel/Effects/ViewModelJumpOffsetEffect.cs
@@ -5,8 +5,14 @@ namespace Woosh.Espionage;
 
 public sealed class ViewModelJumpOffsetEffect : ObservableEntityComponent<CompositedViewModel>, IViewModelEffect
 {
-	public float RotMulti { get; set; } = 1;
+	public float RotMulti { get; set; } = 2;
 	public float PosMulti { get; set; } = 1;
+
+	protected override void OnAutoRegister()
+	{
+		base.OnAutoRegister();
+		// When we have a OnLandedEvent, we should play a bounce animation here.
+	}
 
 	private float m_Offset;
 
@@ -15,7 +21,7 @@ public sealed class ViewModelJumpOffsetEffect : ObservableEntityComponent<Compos
 		m_Offset = m_Offset.LerpTo( Game.LocalPawn.Velocity.z / 75, 15 * Time.Delta );
 		m_Offset = m_Offset.Clamp( -8, 8 );
 
-		setup.Hands.Offset += setup.Rotation.Up * m_Offset * RotMulti;
-		setup.Hands.Angles *= Rotation.From( m_Offset * PosMulti, 0, 0 );
+		setup.Hands.Offset += setup.Rotation.Up * (m_Offset / 2f) * PosMulti;
+		setup.Hands.Angles *= Rotation.From( m_Offset * RotMulti, 0, 0 );
 	}
 }
