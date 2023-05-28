c14af4c758edc61aac199bd0b920f95b176044ac
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2023-05-28 13:28:38 +1000

    Refine Movement Speed

--- Code/ViewModel/Effects/ViewModelMoveBobEffect.cs
+++ Code/ViewModel/Effects/ViewModelMoveBobEffect.cs
@@ -13,7 +13,7 @@ public sealed class ViewModelMoveBobEffect : ObservableEntityComponent<Composite
 
 	public void OnPostCameraSetup( ref CameraSetup setup )
 	{
-		var speed = Entity.Owner.Velocity.WithZ( 0 ).Length.LerpInverse( 0, 180 );
+		var speed = Entity.Owner.Velocity.WithZ( 0 ).Length.LerpInverse( 0, 160 );
 		m_Speed = m_Speed.LerpTo( speed, 4 * Time.Delta );
 		m_Scale = m_Scale.LerpTo( Game.LocalPawn.GroundEntity != null ? speed : 0, 10 * Time.Delta );
 		m_Delta += Time.Delta * 15.0f * m_Scale;
