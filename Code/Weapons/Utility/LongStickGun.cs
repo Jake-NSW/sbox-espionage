cf07707c103d8e42087be08cf3a12cc568bd18f4
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2023-07-03 20:44:12 +1000

    LongStickGun.cs

---
+++ Code/Weapons/Utility/LongStickGun.cs
@@ -0,0 +1,85 @@
+﻿using Editor;
+using Sandbox;
+using Woosh.Common;
+using Woosh.Signals;
+
+namespace Woosh.Espionage.Utility;
+
+[Library( "esp_long_stick_gun" ), HammerEntity, EditorModel( WORLD_MODEL )]
+public sealed class LongStickGun : ObservableAnimatedEntity, ICarriable, IPickup, ISlotted<CarrySlot>, IHave<EntityInfo>
+{
+	private const string WORLD_MODEL = "weapons/mk23/espionage_mk23.vmdl";
+
+	public LongStickGun()
+	{
+		if ( !Game.IsClient )
+			return;
+
+		Events.Register<CreatedViewModel>(
+			static evt => evt.Data.ViewModel.Build()
+				.WithModel( Model.Load( "weapons/debug/v_long_gun.vmdl" ) )
+				.WithAspect( new ViewModelEffectsAspect() )
+				.WithComponent( new GenericFirearmViewModelAnimator() )
+				.WithMaterialGroup( "chrome" )
+				.WithBodyGroup( "muzzle", 0 )
+				.WithBodyGroup( "module", 1 )
+		);
+	}
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Tags.Add( "pickup" );
+
+		PhysicsEnabled = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+
+		Model = Cloud.Model( "woosh.mdl_esp_mk23" );
+		SetupPhysicsFromModel( PhysicsMotionType.Dynamic );
+	}
+
+	public CarrySlot Slot => CarrySlot.Utility;
+
+	public EntityInfo Item => new EntityInfo()
+	{
+		Display = "Long Stick Gun",
+		Brief = "A gun with a long stick",
+		Icon = "gavel",
+	};
+
+	// IPickup
+
+	void IPickup.OnPickup( Entity carrier )
+	{
+		EnableAllCollisions = false;
+	}
+
+	void IPickup.OnDrop()
+	{
+		EnableAllCollisions = true;
+
+		var down = Rotation.LookAt( Owner.AimRay.Forward ).Down;
+		Position = Owner.AimRay.Position + down * 24;
+		Velocity += down * 12;
+	}
+
+	// ICarriable
+
+	public DrawTime Draw => new DrawTime( 1, 0.6f );
+
+	void ICarriable.Deploying()
+	{
+		if ( Game.IsServer )
+			EnableDrawing = true;
+	}
+
+	void ICarriable.Holstering( bool drop ) { }
+
+	void ICarriable.OnHolstered()
+	{
+		if ( Game.IsServer )
+			EnableDrawing = false;
+	}
+}
